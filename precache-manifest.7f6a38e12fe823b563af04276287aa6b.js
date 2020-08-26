c5d0c1ecd5a02a0bb1667298e3107cd489c6afb1
Anusha Banerjee <rairaianusha@gmail.com>
2020-08-26 20:56:33 +0530

    Updates

---
+++ precache-manifest.7f6a38e12fe823b563af04276287aa6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e1d93dce633844c3e4d5010090c019a",
+    "url": "/webdev-image-gallery/index.html"
+  },
+  {
+    "revision": "bb6f4ead3033e6fc021c",
+    "url": "/webdev-image-gallery/static/css/main.356577a9.chunk.css"
+  },
+  {
+    "revision": "9dda61e924de8108e8cd",
+    "url": "/webdev-image-gallery/static/js/2.053fab12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webdev-image-gallery/static/js/2.053fab12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb6f4ead3033e6fc021c",
+    "url": "/webdev-image-gallery/static/js/main.350215cc.chunk.js"
+  },
+  {
+    "revision": "0348822a207e8cb3e5e0",
+    "url": "/webdev-image-gallery/static/js/runtime-main.d80c18b0.js"
+  }
+]);
